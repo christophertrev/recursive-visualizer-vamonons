d9f1ceb83d5dd4d233c6c4a84d0360678c5b6585
Christopher Treviño <christophertrev@gmail.com>
2015-02-14 15:36:48 -0800

    (Feat) Add code example and entry fil

---
+++ createHTML/exampleCode/example1.js
@@ -0,0 +1,9 @@
+var recursiveReverse = function (str){
+  if( str.length === 1 ){
+    return str;
+  }
+  var result  = str.slice(-1);
+  result += recursiveReverse(str.slice(0,-1));
+  return result;
+
+}
