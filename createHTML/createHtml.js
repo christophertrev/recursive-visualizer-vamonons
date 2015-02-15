89edfe58d3afda6e1911bd8f3a52e029f93995b5
Christopher Treviño <christophertrev@gmail.com>
2015-02-14 16:13:23 -0800

    (feat) Read in file

--- createHTML/createHtml.js
+++ createHTML/createHtml.js
@@ -1,3 +1,23 @@
 var fs  = require('fs');
 var arguments = process.argv.slice(2);
-console.log(arguments)
+// console.log(arguments)
+
+fs.readFile(__dirname +'/' +arguments[0], 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  // data.split('/n')
+  // console.log(data.split('\n'));
+  createPseudoCode(data)
+});
+
+
+var keywords= ['var', 'function','if', 'return']
+
+var createPseudoCode = function (codeStr){
+
+  console.log(codeStr.split(/\n/))
+}
+
+
+
