5b87f4ca839d2a681884293e77666cba5dcbb735
Christopher Treviño <christophertrev@gmail.com>
2015-02-14 16:28:25 -0800

    (feat) makes sudo code correctly

--- createHTML/createHtml.js
+++ createHTML/createHtml.js
@@ -8,15 +8,22 @@ fs.readFile(__dirname +'/' +arguments[0], 'utf8', function (err,data) {
   }
   // data.split('/n')
   // console.log(data.split('\n'));
-  createPseudoCode(data)
+  //Create object to place into PsudeoCode area
+  var results = {}
+  results.pseudoCode = createPseudoCode(data);
+  // console.log('end Product', createPseudoCode(data))
+  console.log(results)
 });
 
 
 var keywords= ['var', 'function','if', 'return']
 
 var createPseudoCode = function (codeStr){
-
-  console.log(codeStr.split(/\n/))
+  // Remove var key words and { or }
+  var results = codeStr.replace(/var |{|}/g,'')
+  // Remove empty Lines
+  results = results.replace(/^\s*[\r\n]/gm,'')
+  return results
 }
 
 
--- createHTML/exampleCode/example1.js
+++ createHTML/exampleCode/example1.js
@@ -2,7 +2,7 @@ var recursiveReverse = function (str){
   if( str.length === 1 ){
     return str;
   }
-  var result  = str.slice(-1);
+  var result = str.slice(-1);
   result += recursiveReverse(str.slice(0,-1));
   return result;
 
